cf422d5330b8bcf2c66c99ad67b559b0f2b6019b
Harsh Gupta <harsh.gupta1813@gmail.con>
2023-12-15 01:41:18 +0530

    fronted reset

--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,4 +1,6 @@
-        # img= Image.open(r"Python\Images\logo-9.png")
-        # img= img.resize((500,130), Image.LANCZOS)
-        # self.photoimg= ImageTk.PhotoImage(img)
+img= Image.open(r"Python\Images\logo-9.png")
+        img= img.resize((425,100), Image.LANCZOS)
+        self.photoimg= ImageTk.PhotoImage(img)
       
+        f_label= Label(self.root, image= self.photoimg)
+        f_label.place(x=0, y=0, width=425, height= 100)
